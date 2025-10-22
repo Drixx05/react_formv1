c84bf8ffb15471a211a47783f21230b5d1e3bfed
Antonin Grenier <drixion05@gmail.com>
2025-10-22 10:51:14 +0200

    fixed error handling on forms

--- src/App.jsx
+++ src/App.jsx
@@ -7,81 +7,77 @@ import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset,
-    } = useForm({
-        defaultValues: {
-            name: "",
-            dueDate: "",
-            priority: "low",
-            isChecked: false,
-        },
-    });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm({
+		defaultValues: {
+			name: "",
+			dueDate: "",
+			priority: "low",
+			isChecked: false,
+		},
+	});
 
-    const onSubmit = (data) => {
-        console.log(data);
-        reset();
-    };
+	const onSubmit = (data) => {
+		console.log(data);
+		reset();
+	};
 
-    return (
-        <Container as="main" className="py-5 d-flex justify-content-center">
-            <Row className="justify-content-center ">
-                <Col>
-                    <h2 className="mb-4 text-center">Formulaire de tâche</h2>
-                    <Form onSubmit={handleSubmit(onSubmit)}>
-                        <Form.Group className="mb-3" controlId="formBasicName">
-                            <Form.Label>Nom</Form.Label>
-                            <Form.Control
-                                type="text"
-                                {...register("name", { required: "Le nom est requis" })}
-                                placeholder="Renseigner le nom de la tâche"
-                                isInvalid={!!errors.name}
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                {errors.name?.message}
-                            </Form.Control.Feedback>
-                        </Form.Group>
+	return (
+		<Container as="main" className="py-5 d-flex justify-content-center">
+			<Row className="justify-content-center ">
+				<Col>
+					<h1 className="mb-4 text-center">Formulaire de tâche</h1>
+					<Form onSubmit={handleSubmit(onSubmit)}>
+						<Form.Group className="mb-3" controlId="formBasicName">
+							<Form.Label>Nom</Form.Label>
+							<Form.Control
+								type="text"
+								{...register("name", { required: "Le nom est requis" })}
+								placeholder="Renseigner le nom de la tâche"
+								isInvalid={!!errors.name}
+							/>
+							{errors.name && (<Form.Control.Feedback type="invalid">{errors.name?.message}</Form.Control.Feedback>)}
+						</Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicDate">
-                            <Form.Label>Date</Form.Label>
-                            <Form.Control
-                                type="date"
-                                {...register("dueDate", { required: "La date est requise" })}
-                                isInvalid={!!errors.dueDate}
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                {errors.dueDate?.message}
-                            </Form.Control.Feedback>
-                        </Form.Group>
+						<Form.Group className="mb-3" controlId="formBasicDate">
+							<Form.Label>Date</Form.Label>
+							<Form.Control
+								type="date"
+								{...register("dueDate", { required: "La date est requise" })}
+								isInvalid={!!errors.dueDate}
+							/>
+              {errors.dueDate && (<Form.Control.Feedback type="invalid">{errors.dueDate?.message}</Form.Control.Feedback>)}
+						</Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPriority">
-                            <Form.Label>Priorité</Form.Label>
-                            <Form.Select {...register("priority")}>
-                                <option value="low">Basse</option>
-                                <option value="medium">Moyenne</option>
-                                <option value="high">Haute</option>
-                            </Form.Select>
-                        </Form.Group>
+						<Form.Group className="mb-3" controlId="formBasicPriority">
+							<Form.Label>Priorité</Form.Label>
+							<Form.Select {...register("priority")}>
+								<option value="low">Basse</option>
+								<option value="medium">Moyenne</option>
+								<option value="high">Haute</option>
+							</Form.Select>
+						</Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check
-                                type="checkbox"
-                                label="Tâche terminée"
-                                {...register("isChecked")}
-                            />
-                        </Form.Group>
+						<Form.Group className="mb-3" controlId="formBasicCheckbox">
+							<Form.Check
+								type="checkbox"
+								label="Tâche terminée"
+								{...register("isChecked")}
+							/>
+						</Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            Envoyer
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-    );
+						<Button variant="primary" type="submit">
+							Envoyer
+						</Button>
+					</Form>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
